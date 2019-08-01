aebfcf1bc78701dab9c6a64471366d4035e124eb
Emily Reinhart <em.j.reinhart@gmail.com>
2019-07-31 23:38:04 -0500

    add happyhour page

---
+++ script.js
@@ -0,0 +1,18 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.3.3/firebase-app.js"></script>
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#config-web-app -->
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDcQzxmLn5YHNhjdzyU2thgVByFIyOU3OM",
+    authDomain: "happyhour-2753b.firebaseapp.com",
+    databaseURL: "https://happyhour-2753b.firebaseio.com",
+    projectId: "happyhour-2753b",
+    storageBucket: "",
+    messagingSenderId: "587157477182",
+    appId: "1:587157477182:web:2ddb49c870f7100f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
